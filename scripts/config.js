5b255ccd155045ae8c3e4793e6ea5e49b2aa129c
Terry <lict@novasoftware.cn>
2015-04-02 10:12:36 +0800

    -----

---
+++ scripts/config.js
@@ -0,0 +1,10 @@
+window.Nova ={
+	config:{
+		remoteAddress : 'http://jbis.cloudapp.net/Dev',
+		//remoteAddress : 'http://localhost:81',
+		debug:true,
+		version:null,
+		timeout:20000,
+		syncTime:10000
+	}
+};
