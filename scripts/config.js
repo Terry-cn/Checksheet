afd7e3b8877254f6fbab97d674797646c873fac4
Terry <lict@novasoftware.cn>
2015-05-08 13:52:42 +0800

    -

--- scripts/config.js
+++ scripts/config.js
@@ -1,8 +1,8 @@
 window.Nova ={
 	config:{
 		//remoteAddress : 'http://eprodata.cloudapp.net:8080',
-		remoteAddress : 'http://192.168.191.1:81',
-		//remoteAddress : 'http://www.eprodata.com.au:8080',
+		//remoteAddress : 'http://192.168.191.1:81',
+		remoteAddress : 'http://www.eprodata.com.au:8080',
 
 		debug:true,
 		version:null,
